af1385333a2c6a8130e3dd8071c6902da768190c
Amber Blue <amber.blue.n@gmail.com>
2022-04-29 14:16:01 -0400

    Update parameters

--- Arduino/stepper_functions.ino
+++ Arduino/stepper_functions.ino
@@ -2,33 +2,36 @@
 // Use MIDI Channel 2 for Stepper motor movement
 
 
-// Updated Stepper functions - not midi compatible?? 
-//#include <Stepper.h>
-//#define stepsPerRevolution 5000  // actual = 200 (microstep) 
-//#define STEP_SIZE 10000
+// Updated Stepper functions
+#include <Stepper.h>
+const int stepsPerRevolution = 5000;  // actual = 200 (microstep) 
+const int STEP_SIZE =  5000;
 
-//Stepper stepperY(stepsPerRevolution, 2, 3);
-//Stepper stepperX(stepsPerRevolution, 4, 5);
+Stepper stepperY(stepsPerRevolution, 2, 3);
+
+
+Stepper stepperX(stepsPerRevolution, 4, 5);
 // Move row, single position change
 // @param CURR_ROW - The current row
 // @param NEXT_ROW - The target row 
 // @returns CURR_ROW - the current row
-
 int changeRow(int CURR_ROW, int NEXT_ROW){
-  int STEP_SIZE = 400;
+
+  int STEP_SIZE = 5000;
+  stepperY.setSpeed(100);
   int STEP = abs(NEXT_ROW - CURR_ROW);
     if(CURR_ROW == NEXT_ROW) {
     } else if(CURR_ROW < NEXT_ROW) {
         Serial.println("forward:" + STEP);
         stepperY.step(STEP*STEP_SIZE);
         CURR_ROW = NEXT_ROW;
-        delay(500);
+        delay(1000);
         
     } else {
         Serial.println("back:" + STEP);
         stepperY.step(-(STEP*STEP_SIZE));
         CURR_ROW = NEXT_ROW;
-        delay(500);
+        delay(1000);
     }
 
     return CURR_ROW;
@@ -38,7 +41,7 @@ int changeRow(int CURR_ROW, int NEXT_ROW){
 int moveRow(int CURR_ROW, int NEXT_ROW) {
    Serial.print(NEXT_ROW);
     Serial.print('\n');
-    delay(500);
+    delay(1000);
     int STEP = abs(NEXT_ROW - CURR_ROW);
     if(CURR_ROW != NEXT_ROW) {
       if(CURR_ROW < NEXT_ROW) {
@@ -58,11 +61,11 @@ int moveRow(int CURR_ROW, int NEXT_ROW) {
    return CURR_ROW;
 }
 
-// Move rows, array or positions up to 100
+// Move rows, array or positions up to 10
 // @param positions - array of row positions 
 // @returns CURR_ROW - the current row
-int moveRows(uint8_t positions[100]) {
-  for (byte i = 0; i < sizeof(positions); i = i + 1) {
+int moveRows(uint8_t positions[10], int CURR_ROW) {
+  for (byte i = 0; i < 11; i = i + 1) {
     int NEXT_ROW = positions[i];
     Serial.print(NEXT_ROW);
     Serial.print('\n');
@@ -114,34 +117,35 @@ int moveCol(int CURR_COL, int NEXT_COL) {
    return CURR_COL;
 }
 
-// Move columns, array or positions up to 100
+// Move columns, array or positions up to 10
 // @param positions - array of column positions 
 // @returns CURR_COL - the current column
-int moveCols(uint8_t positions[100]) {
-  for (byte i = 0; i < sizeof(positions); i = i + 1) {
-    int NEXT_COL = positions[i];
-    Serial.print(NEXT_COL);
-    Serial.print('\n');
-    delay(500);
-    int STEP = abs(NEXT_COL - CURR_COL);
-    if(CURR_COL == NEXT_COL) {
-      continue;
-    } else if(CURR_COL < NEXT_COL) {
-        Serial.println("forward:" + STEP);
-        stepperY.step(STEP*STEP_SIZE);
-        CURR_COL = NEXT_COL;
-        delay(500);
-        
-    } else {
-       Serial.println("back:" + STEP);
-        stepperY.step(-(STEP*STEP_SIZE));
-        CURR_COL = NEXT_COL;
-        delay(500);
-    }
-  }
-
-  return CURR_COL;
-}
+//int moveCols(uint8_t positions[], int CURR_COL) {
+// // size_t movements = sizeof(positions)/sizeof(positions[0]);
+//  for (byte i = 0; i < 11; i = i + 1) {
+//    int NEXT_COL = positions[i];
+//    Serial.print(NEXT_COL);
+//    Serial.print('\n');
+//    delay(500);
+//    int STEP = abs(NEXT_COL - CURR_COL);
+//    if(CURR_COL == NEXT_COL) {
+//      continue;
+//    } else if(CURR_COL < NEXT_COL) {
+//        Serial.println("forward:" + STEP);
+//        stepperY.step(STEP*STEP_SIZE);
+//        CURR_COL = NEXT_COL;
+//        delay(500);
+//        
+//    } else {
+//       Serial.println("back:" + STEP);
+//        stepperY.step(-(STEP*STEP_SIZE));
+//        CURR_COL = NEXT_COL;
+//        delay(500);
+//    }
+//  }
+//
+//  return CURR_COL;
+//}
 
 
 // Move # of steps in x direction
@@ -180,34 +184,34 @@ int moveCols(uint8_t positions[100]) {
 
 // =============================================================================
 //Move in the X-direction
-void moveX(uint8_t dirSig, int stepsPerRev, int speedMicroSec) {
-  // Set motor direction: HIGH for clockwise, LOW for counterclockwise
-  digitalWrite(DIR_PIN_X, dirSig);
- 
-  // Spin motor quickly
-  for(int x = 0; x < stepsPerRev; x++)
-  {
-    digitalWrite(STEP_PIN_X, HIGH);
-    delayMicroseconds(speedMicroSec);
-    digitalWrite(STEP_PIN_X, LOW);
-    delayMicroseconds(speedMicroSec);
-  }
-}
-
-// Move in the Y-direction
-void moveY(uint8_t dirSig, int stepsPerRev, int speedMicroSec) {
-  // Set motor direction: HIGH for clockwise, LOW for counterclockwise
-  digitalWrite(DIR_PIN_Y, dirSig);
- 
-  // Spin motor quickly
-  for(int x = 0; x < stepsPerRev; x++)
-  {
-    digitalWrite(STEP_PIN_Y, HIGH);
-    delayMicroseconds(speedMicroSec);
-    digitalWrite(STEP_PIN_Y, LOW);
-    delayMicroseconds(speedMicroSec);
-  }
-}
+//void moveX(uint8_t dirSig, int stepsPerRev, int speedMicroSec) {
+//  // Set motor direction: HIGH for clockwise, LOW for counterclockwise
+//  digitalWrite(DIR_PIN_X, dirSig);
+// 
+//  // Spin motor quickly
+//  for(int x = 0; x < stepsPerRev; x++)
+//  {
+//    digitalWrite(STEP_PIN_X, HIGH);
+//    delayMicroseconds(speedMicroSec);
+//    digitalWrite(STEP_PIN_X, LOW);
+//    delayMicroseconds(speedMicroSec);
+//  }
+//}
+//
+//// Move in the Y-direction
+//void moveY(uint8_t dirSig, int stepsPerRev, int speedMicroSec) {
+//  // Set motor direction: HIGH for clockwise, LOW for counterclockwise
+//  digitalWrite(DIR_PIN_Y, dirSig);
+// 
+//  // Spin motor quickly
+//  for(int x = 0; x < stepsPerRev; x++)
+//  {
+//    digitalWrite(STEP_PIN_Y, HIGH);
+//    delayMicroseconds(speedMicroSec);
+//    digitalWrite(STEP_PIN_Y, LOW);
+//    delayMicroseconds(speedMicroSec);
+//  }
+//}
 
 
 
